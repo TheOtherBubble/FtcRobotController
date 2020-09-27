2681273a8aeb56e6d6b04c2e4e239f8c563b9393
Viridian Robotics <viridianrobotics@gmail.com>
2020-09-27 13:00:12 -0500

    Auton test classes for mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/AutonDriveFunctionsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/AutonDriveFunctionsTest.java
@@ -56,7 +56,7 @@ import org.firstinspires.ftc.teamcode.Hardware;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Auton Driving Test", group="Pushbot")
+@Autonomous(name="Auton Driving Test With Function", group="Pushbot")
 //@Disabled
 public class AutonDriveFunctionsTest extends LinearOpMode {
 
