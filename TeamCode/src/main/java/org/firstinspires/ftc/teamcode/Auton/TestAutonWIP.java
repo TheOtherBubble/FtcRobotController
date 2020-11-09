612a6d39f71bdd9e20423470ad7d41df11558aa8
Subramanian-Ria <riasub08@gmail.com>
2020-11-09 12:25:56 -0600

    added some comments on nature of WIP errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/TestAutonWIP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/TestAutonWIP.java
@@ -14,14 +14,16 @@ public class TestAutonWIP extends AutonDrivingWIP {
         //init
         robot.init(hardwareMap);
         initImu();
-        initVuforia();
+        initVuforia(); //may not work unsure
         stopAndReset();
 
         waitForStart();
 
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
 
-        vuforia(allTrackables, targetsUltimateGoal, 500);
+        vuforia(allTrackables, targetsUltimateGoal, 500); //may not work unsure
+
+        //probably some kind of vuforia error bc throws null pointer upon running
 
         pathComplete(500);
     }
